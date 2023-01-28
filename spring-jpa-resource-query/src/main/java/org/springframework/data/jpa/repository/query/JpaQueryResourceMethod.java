5f693dbabcf648618df1cf5a920f253d125cfc28
Can <mcanataly@gmail.com>
2023-01-28 18:15:25 +0300

    JpaQueryResourceMethod
    This extended version of JpaQueryMethod will replace "getAnnotatedQuery" and "getRequiredAnnotatedQuery" results with actual query if @Query value parameter starts with classpath:

---
+++ spring-jpa-resource-query/src/main/java/org/springframework/data/jpa/repository/query/JpaQueryResourceMethod.java
@@ -0,0 +1,52 @@
+package org.springframework.data.jpa.repository.query;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.data.jpa.provider.QueryExtractor;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+
+public class JpaQueryResourceMethod extends JpaQueryMethod {
+    private static final ResourceLoader RESOURCE_LOADER = new DefaultResourceLoader();
+
+    protected JpaQueryResourceMethod(Method method, RepositoryMetadata metadata, ProjectionFactory factory, QueryExtractor extractor) {
+        super(method, metadata, factory, extractor);
+    }
+
+    @Override
+    String getAnnotatedQuery() {
+        String value = super.getAnnotatedQuery();
+        if (isResourcePath(value)) return getAnnotatedQueryFromResource(value);
+        return value;
+    }
+
+    @Override
+    String getRequiredAnnotatedQuery() throws IllegalStateException {
+        String value = super.getAnnotatedQuery();
+        if (isResourcePath(value)) return getAnnotatedQueryFromResource(value);
+        return value;
+    }
+
+    private boolean isResourcePath(@Nullable String value) {
+        return value != null && value.toLowerCase().startsWith("classpath:");
+    }
+
+    private String getAnnotatedQueryFromResource(@NonNull String path) {
+        Resource resource = RESOURCE_LOADER.getResource(path);
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
